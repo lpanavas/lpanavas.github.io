65cfd1b0e8af52018b53b5569a16a91a662622ee
lpanavas <71109392+lpanavas@users.noreply.github.com>
2022-10-27 14:06:42 -0400

    Updates

---
+++ precache-manifest.18c34433277674ccdfb7113e789337d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3451266285c77796b2412f6994b6e93",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56b78ef07ac938f67031",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "348aa0a0bfccc03d8de4",
+    "url": "/static/js/2.267342fc.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.267342fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56b78ef07ac938f67031",
+    "url": "/static/js/main.4bccf51d.chunk.js"
+  },
+  {
+    "revision": "b4ba650b238755144e2a",
+    "url": "/static/js/runtime-main.e5fc83a6.js"
+  }
+]);
