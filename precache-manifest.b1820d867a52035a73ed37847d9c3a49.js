b03da3aa423f76e45c60b65544934bcd4aef556a
lpanavas <71109392+lpanavas@users.noreply.github.com>
2022-10-27 14:58:51 -0400

    Updates

--- precache-manifest.b1820d867a52035a73ed37847d9c3a49.js
+++ precache-manifest.b1820d867a52035a73ed37847d9c3a49.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2d2d7190fd66639b1439a51b93e2ea31",
+    "revision": "69e918a79b29c8c0404fb411f381acdb",
     "url": "/index.html"
   },
   {
-    "revision": "56f72536cb62b61fd9a0",
+    "revision": "95a73d4580700a2360d3",
     "url": "/static/css/main.5ecd60fb.chunk.css"
   },
   {
-    "revision": "b7420fb7a01e6741123e",
-    "url": "/static/js/2.98b64498.chunk.js"
+    "revision": "d4a55842113422765edb",
+    "url": "/static/js/2.e94c3c26.chunk.js"
   },
   {
     "revision": "3453b8997016469371284a28c0e873e2",
-    "url": "/static/js/2.98b64498.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.e94c3c26.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "56f72536cb62b61fd9a0",
-    "url": "/static/js/main.8a55d7ed.chunk.js"
+    "revision": "95a73d4580700a2360d3",
+    "url": "/static/js/main.f50a731f.chunk.js"
   },
   {
     "revision": "b4ba650b238755144e2a",
