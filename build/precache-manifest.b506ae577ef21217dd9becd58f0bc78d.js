cf1a859b67d8b0761c0337f0e593fecf6411943d
Liudas Panavas <panavas.l@northeastern.edu>
2023-11-29 13:21:56 -0500

    new changes

---
+++ build/precache-manifest.b506ae577ef21217dd9becd58f0bc78d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d406b0386ec65004293871a46cb12439",
+    "url": "/index.html"
+  },
+  {
+    "revision": "230f065e230a9400a646",
+    "url": "/static/css/main.313fb038.chunk.css"
+  },
+  {
+    "revision": "0e283b8e93793491b028",
+    "url": "/static/js/2.b1ddf634.chunk.js"
+  },
+  {
+    "revision": "4c25d06d95384d1d9cff7a908ed8d725",
+    "url": "/static/js/2.b1ddf634.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "230f065e230a9400a646",
+    "url": "/static/js/main.762be744.chunk.js"
+  },
+  {
+    "revision": "430f72daf7c3a4d42642",
+    "url": "/static/js/runtime-main.351e9c47.js"
+  },
+  {
+    "revision": "1b7e20740bf5c2d871e303e8e86841d0",
+    "url": "/static/media/LiudasPanavasResume.1b7e2074.pdf"
+  }
+]);
